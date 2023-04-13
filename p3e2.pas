25084797444506948f7aefc889aa23b71f3afec3
FedeCF03 <fedecf03@gmail.com>
2023-04-12 21:03:22 -0300

    cadp

---
+++ p3e2.pas
@@ -0,0 +1,40 @@
+
+Program p3e3;
+
+Type 
+
+  escuela = 
+            Record
+
+              cue: integer;
+              nameest: string;
+              cantdocentes: integer;
+              cantalum: integer;
+              localidad: string;
+            End;
+
+
+Var ecuela: escuela;
+
+Var cantescuelas,cue1,cue2: integer;
+Procedure asignacion ();
+Begin
+  WriteLn('inserte cue');
+  ReadLn(ecuela.cue);
+  WriteLn('nombre del establecimiento');
+  ReadLn(ecuela.nameest);
+  WriteLn('cant docentes');
+  ReadLn(ecuela.cantdocentes);
+  WriteLn('cantidad de alumnos');
+  ReadLn(ecuela.cantalum);
+  WriteLn('localidad');
+  ReadLn(ecuela.localidad);
+
+End;
+
+Procedure 
+
+
+Begin
+
+End.
