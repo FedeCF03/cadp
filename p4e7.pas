e49c538a3f5f7446160008a4f2d89d8f9032c286
FedeCF03 <fedecf03@gmail.com>
2023-04-26 17:03:20 -0300

    p4e7 p4e7

--- p4e7.pas
+++ p4e7.pas
@@ -12,6 +12,12 @@ Begin
  numero := numero Div 10;
 
 End;
+Procedure oculorrencia(digito,numero:integer;Var a:numeros);
+Begin
+ If Not(numero=0)Then
+  a[digito] := a[digito]+1;
+
+End;
 Procedure inicializararray(Var a:numeros);
 
 Var i:integer;
@@ -21,12 +27,41 @@ Begin
    a[i] := 0;
   End;
 End;
+Procedure degetomsleido(pito:numeros;Var concha,p :integer);
 
+Var i:integer;
+Begin
+ For i:=0 To 9 Do
+  Begin
+   If (pito[i]>concha)Then
+    Begin
+     concha := pito[i];
+     p := i;
+    End;
+  End;
+End;
+
+Function degetosemocuremsa(a:numeros):integer;
+
+Var i:integer;
+Begin
+ For i:=0 To 9 Do
+  Begin
+   If (a[i]=0) Then WriteLn(i,'no tuvo concurrencias, como vos ');
+
+
+
+  End;
+
+
+End;
 
 Var 
- numero,digito:integer;
+ numero,digito,x,p:integer;
  a:numeros;
 Begin
+ x := 0;
+ p := 0;
  inicializararray(a);
  digito := 0;
  ReadLn(numero);
@@ -35,11 +70,15 @@ Begin
    While Not(numero=0) Do
     Begin
      separardigitos(numero,digito);
+     oculorrencia(digito,numero,a);
+
 
     End;
+   ReadLn(numero);
 
 
   End;
-
-
+ degetomsleido(a,x,p);
+ degetosemocuremsa(a);
+ WriteLn(p);
 End.
