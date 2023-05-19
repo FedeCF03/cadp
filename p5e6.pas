6aa63075cdd0f180bde69d378663a2e1bff432fe
Federico Caltabiano <97410583+FedeCF03@users.noreply.github.com>
2023-05-19 11:52:24 -0300

    Update p5e6.pas

--- p5e6.pas
+++ p5e6.pas
@@ -8,8 +8,8 @@ Type
  sondas = Record
   name:srt;
   duracion:integer;
-  costoc:real;
-  costom:real;
+  costoc:real; // costo construcción 
+  costom:real; //costo mensual 
   rango:cats;
  End;
 
@@ -75,7 +75,7 @@ Begin
   End;
 End;
 
-Procedure mascara(Var nsmascara:srt; Var costoc,costom:real;dato:sondas);
+Procedure mascara(Var nsmascara:srt; Var costoc,costom:real;dato:sondas);  // construcción más cara
 
 
 Begin
@@ -124,7 +124,7 @@ Begin
 
 End;
 
-Procedure recorrerLista(l:lista);
+Procedure recorrerLista(l:lista;var promedioc: real;promedios:integer;);
 
 Var 
  secun:lista;
