4d52dc9478a03577033328d682a7785311390662
FedeCF03 <fedecf03@gmail.com>
2023-05-25 21:00:32 -0300

    12

--- p6e11.pas
+++ p6e11.pas
@@ -17,4 +17,83 @@ Type
 
  End;
 
- valu = array [0..10] Of alumno;
+Procedure leeralumno(a:alumno);
+
+Begin
+
+ ReadLn(a.nroalu);
+ If Not(a.nroalu=0)Then
+  Begin
+   ReadLn(a.apellido);
+   ReadLn(a.promedio);
+  End;
+
+End;
+
+Procedure insertarordenado(Var l:lista; a:alumno);
+
+Var nuevo,act,ant:lista;
+
+Begin
+ new(nuevo);
+ nuevo^.dato = a;
+ act := l;
+ ant := l;
+
+ While Not(act=Nil) And Not(a.promedio>act^.dato.promedio) Do
+  Begin
+   ant := act;
+   act := act^.sig;
+  End;
+
+ If (act=ant)Then
+  Begin
+   l := nuevo;
+  End
+ Else
+  ant^.sig = nuevo;
+ nuevo^.sig := act;
+End;
+
+Procedure cargaralumno(Var l:lista);
+
+Var a:alumno;
+
+Begin
+ leeralumno(a);
+ While Not(a.nroalu=0) Do
+  Begin
+   insertarordenado(l,a);
+   leeralumno(a);
+  End;
+End;
+
+Procedure premio(l:lista);
+
+Var i :integer
+       Begin
+
+        For i:=0 To 9 Do
+         Begin
+          l^.dato.apellido;
+          WriteLn( l^.dato.nroalu,' ',l^.dato.nroalu);
+
+          l^.sig;
+
+
+
+
+         End;
+
+
+       End;
+
+Var l:lista;
+
+Begin
+ l := Nil;
+ cargaralumno(l);
+ premio(l);
+
+
+End.
